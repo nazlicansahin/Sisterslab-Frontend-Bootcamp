7542ab80fdd3070046c75582a0c836f539568975
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2023-10-14 01:50:58 +0300

    Homework done

--- Week1/docs/assignment.js
+++ Week1/docs/assignment.js
@@ -1,48 +1,54 @@
+/*
 let a = 2;
 let b = 3;
 let c = 2;
-(a == b) // returns ?
-(a == c) //returns ?
-
-typeof "John Doe" // Returns ?
-typeof 3.14 // Returns ?
-typeof true // Returns ?
-typeof 234567890123456789012345678901234567890n // Returns ?
-typeof undefined // Returns ?
-typeof null // Returns ?
-typeof Symbol('symbol') // Returns ?
+(a == b) // returns ?  >>3
+(a == c) //returns ? >>2
+*/
+typeof "John Doe" // Returns ?  >>string
+typeof 3.14 // Returns ?    >>number
+typeof true // Returns ?    >>boolean
+typeof 234567890123456789012345678901234567890n // Returns ? >>bigint
+typeof undefined // Returns ?  >>undefined
+typeof null // Returns ?  >>object
+typeof Symbol('symbol') // Returns ? >>symbol
 
 let x = 2;
 let y = "2";
-(x == y) // Returns ?
-(x === y) // Returns ?
+/*
+(x == y) // Returns ? >>true
+(x === y) // Returns ?  >>false  
 
 let x = 3;
 let y = "3";
-x + y // Returns ?
+x + y // Returns ?  >>33
 
 let x = 24;
 let y = "Hello";
-x + y // Returns "?
+x + y // Returns "?  >>24Hello
 
 let name = "Vivek";
 let surname = " Bisht";
-name + surname // Returns ?
+name + surname // Returns ?  >>Vivek Bisht
 
 let x = 3;
 let y = "3";
-x - y //Returns ?
+x - y //Returns ? >>0
 
 let x = 0;
 let y = 23;
 
-if(x) { console.log(x) } //?
+if(x) { console.log(x) } //? >>
 
-if(y) { console.log(y) } //?
-
-isNaN("Hello") // Returns ?
-isNaN(345) // Returns ?
-isNaN('1') // Returns ?
+if(y) { console.log(y) } //? >>23
+*/
+isNaN("Hello") // Returns ?  >>true
+isNaN(345) // Returns ?  >>false
+isNaN('1') // Returns ?  >>false
 isNaN(true) // Returns ?
-isNaN(false) // Returns ?
-isNaN(undefined) // Returns ?
+isNaN(false) // Returns ?  >>false
+isNaN(undefined) // Returns ?    >>true
+
+console.log(isNaN("Hello"), isNaN(345), isNaN('1'), isNaN(true), isNaN(false), isNaN(undefined))
+console.log(typeof "John Doe", typeof 3.14, typeof true, typeof 234567890123456789012345678901234567890n, typeof undefined, typeof null, typeof Symbol('symbol'))   
+console.log(x === y)
